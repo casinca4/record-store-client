3e238c33863580fc7083f4adf86508bf75af3f7c
casinca4 <s.antonelli@web.de>
2020-02-03 15:47:26 +0100

    Set up react-router basics

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../scss/App.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Login from './Login';
+import SignUp from './SignUp';
+import Home from './Home';
+
+function App() {
+  return (
+    <div id="app">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div >
+  );
+}
+
+export default App;
